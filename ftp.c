b2b8e6ae908bb75789f4938d022989b594298533
Sergi Granell <xerpi.g.12@gmail.com>
2016-01-22 23:31:21 +0100

    Fix ftp client linked list bug

--- ftp.c
+++ ftp.c
@@ -740,7 +740,7 @@ static void client_list_add(ClientInfo *client)
 		client->next = NULL;
 	} else {
 		client->next = client_list;
-		client->next->prev = client;
+		client_list->prev = client;
 		client->prev = NULL;
 		client_list = client;
 	}
@@ -759,6 +759,11 @@ static void client_list_delete(ClientInfo *client)
 	if (client->next) {
 		client->next->prev = client->prev;
 	}
+	if (client == client_list) {
+		client_list = client->next;
+	}
+
+	number_clients--;
 
 	sceKernelUnlockMutex(client_list_mtx, 1);
 }
