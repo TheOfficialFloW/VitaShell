5e37f5c2da9dff8b3ffddeb0ea595a9ca79efc0e
ribbid987 <yb9647@gmail.com>
2016-09-03 20:22:17 +0200

    swap scePowerIsLowBattery and scePowerIsBatteryCharging

--- main.c
+++ main.c
@@ -319,11 +319,11 @@ void drawShellInfo(char *path) {
 
 	vita2d_texture *battery_bar_image = battery_bar_green_image;
 
-	if (scePowerIsLowBattery()){
-		battery_bar_image = battery_bar_red_image;
-	} else if (scePowerIsBatteryCharging()) {
+	if (scePowerIsBatteryCharging()) {
 		battery_bar_image = battery_bar_charge_image;
-	}
+	} else if (scePowerIsLowBattery()){
+		battery_bar_image = battery_bar_red_image;
+	} 
 
 	float percent = scePowerGetBatteryLifePercent() / 100.0f;
 
