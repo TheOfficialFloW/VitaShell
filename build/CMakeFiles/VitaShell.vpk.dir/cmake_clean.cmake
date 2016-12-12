9c8ef4fc8335517dafaffa58311aa4f357f1e130
TheFloW <theofficialflow1996@gmail.com>
2016-12-12 20:04:21 +0100

    Rm tmp dir

--- build/CMakeFiles/VitaShell.vpk.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/VitaShell.vpk"
-  "VitaShell.vpk_param.sfo"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/VitaShell.vpk.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
