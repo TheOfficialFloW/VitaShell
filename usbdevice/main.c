1db81e22515346a7e6b66ec041571df0b6432de6
TheOfficialFloW <theofficialflow1996@gmail.com>
2017-01-12 18:35:26 +0100

    Update license

--- usbdevice/main.c
+++ usbdevice/main.c
@@ -1,6 +1,6 @@
 /*
-	usbdevice
-	Copyright (C) 2016, TheFloW
+	VitaShell
+	Copyright (C) 2015-2017, TheFloW
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
