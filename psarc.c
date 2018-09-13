96721e683449d092186d625dee6e301c0aa4b52a
Andrin Bertschi <abertschi@users.noreply.github.com>
2018-09-13 14:17:25 +0200

    Feature/symlink archive (#489)
    * fix bug in archive
    
    * dont keep track of recent files within archives

--- psarc.c
+++ psarc.c
@@ -224,6 +224,7 @@ int fileListGetPsarcEntries(FileList *list, const char *path, int sort) {
     entry->name = malloc(entry->name_length + 1);
     strcpy(entry->name, DIR_UP);
     entry->is_folder = 1;
+    entry->is_symlink = 0;
     entry->type = FILE_TYPE_UNKNOWN;
     fileListAddEntry(list, entry, sort);
   }
@@ -243,6 +244,7 @@ int fileListGetPsarcEntries(FileList *list, const char *path, int sort) {
       if (sceFiosStatSync(NULL, dir.fullPath, &stat) >= 0) {
         FileListEntry *entry = malloc(sizeof(FileListEntry));
         if (entry) {
+          entry->is_symlink = 0;
           entry->is_folder = stat.statFlags & 0x1;
           if (entry->is_folder) {
             entry->name_length = strlen(name) + 1;
